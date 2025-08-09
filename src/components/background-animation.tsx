e80ebdcf17b80e03d0f3d0830d2aa9260b444f9d
Aditya Agrawal <adityaagr2910@gmail.com>
2025-08-09 19:01:37 +0000

    add small animation at bacck matching the vibe

---
+++ src/components/background-animation.tsx
@@ -0,0 +1,12 @@
+export function BackgroundAnimation() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden">
+      <div className="orb"></div>
+      <div className="orb"></div>
+      <div className="orb"></div>
+      <div className="orb"></div>
+      <div className="orb"></div>
+      <div className="orb"></div>
+    </div>
+  );
+}
