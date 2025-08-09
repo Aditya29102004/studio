10166650a8cd7be85768c6c50b1077a0c491a083
Aditya Agrawal <adityaagr2910@gmail.com>
2025-08-09 19:02:17 +0000

    cant see

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased relative">
         <BackgroundAnimation />
         {children}
         <Toaster />
