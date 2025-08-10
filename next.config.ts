9da3f7f4543233e443f7e2f166353007badd10e6
Aditya Agrawal <adityaagr2910@gmail.com>
2025-08-10 06:31:37 +0000

    Error: Your project's URL and Key are required to create a Supabase clie

--- next.config.ts
+++ next.config.ts
@@ -21,10 +21,6 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {},
   },
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
-  },
 };
 
 export default nextConfig;
